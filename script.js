630dd6e8d4c393afe10cd9fc345c9027a85acfa1
cCringy <rofi2402@gmail.com>
2024-07-23 12:24:03 +0200

    First Sort Animation+Navbar layout

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const container = document.getElementById("container");
 const values = [];
 
+// Display Bars when opening the Site
 init();
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// clears the Container div with the bars
 function clearContainer(){
     document.getElementById("container").innerHTML = "";
 }
 
+
+// clears the Container div and generates new Set of bars
 function render(){
     clearContainer();
-
     let size = values.length;
     //display the Array values as bars 
     for(let j = 0;j<=size-1;j++){
@@ -25,6 +29,7 @@ function render(){
     }
 }
 
+// generates random Integer
 // The maximum is exclusive and the minimum is inclusive
 function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
@@ -41,42 +46,54 @@ function assignValues(){
     }
 }
 
+// Fills Array with random values and display new Values as Bars
 function init(){
     assignValues();
     render();
 }
 
+// Creates deep Copy of array and Sorts it to get the swaps
+// which are then animated 
 function play(){
     const copy = [...values];
-    animate(insertionSort(copy));
-
+    swaps = insertionSort(copy);
+    console.log(swaps);
+    animate(swaps);
+    
 }
 
+// Animates the swaps on Container
 function animate(swaps){
     if(swaps.length == 0){return;}
     [i,j] = swaps.shift();
-    [values[i],values[j]] = [values[j],values[i]];
+    swapValues(values,i,j);
     render();
     setTimeout(function(){
         animate(swaps);
     },100);
 }
 
+// exchanges Position of 2 Values in given Array
+function swapValues(a,pos1,pos2){
+    const temp = a[pos1];
+    a[pos1]=a[pos2];
+    a[pos2]=temp;
+}
+
+// Returns all swaps this version of InsertionSort will make on given Array
 function insertionSort(a){
     const swaps = [];
-    let size = a.length-1;
-    for(let i = 0; i<=size;i++){
-        let j = i-1;
-        let key = values[i];
-        while(j >= 0 && key<values[j]){
-            a[j+1]=a[j];
-            swaps.push([j+1,j]);
-            j--;
+    for(let i = 1;i<=a.length;i++){
+        posE = i;
+        x=i-1;
+        while (x>=0 && a[posE]<a[x]){
+            swapValues(a,posE,x);
+            swaps.push([posE,x]);
+            posE--;
+            x--;
         }
-        a[j+1]=key;
-        swaps.push([j+1,i]);
-
     }
+
     return swaps;
 }
 
